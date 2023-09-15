863125697fa7cb2ddb628a88faf2364c74f32758
SeungYun-58 <82879029+SeungYun-58@users.noreply.github.com>
2023-09-16 00:46:56 +0900

    ADD 플레이어 이동 및 점프

--- XRContents8TeamProject/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ XRContents8TeamProject/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,35 +1,79 @@
 using EnemyScripts;
 using UnityEngine;
 
-public class PlayerTest : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     public float MyRadius => rad;
     public float rad;
-    private float moveSpeed = 5f;
+    [SerializeField] private NEnemyController nEnemyController;
+
+    [SerializeField] private float moveSpeed = 5.0f;
+    [SerializeField] private float jumpForce = 5.0f;
+    private bool isJumping;
+
+    public Rigidbody2D playerRigidbody;
+
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
         PlayerMove();
-        PlayerShoot();
-    }
-
-    [SerializeField] private NEnemyController nEnemyController;
-
-    void PlayerShoot()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetButton("Jump"))
         {
-            nEnemyController.TestHIT();
+            PlayerJump();
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+            Debug.Log("JumpingState : " + isJumping);
+        }
+    }
+
+    //void PlayerShoot()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space))
+    //    {
+    //        nEnemyController.TestHIT();
+    //    }
+    //}
+
     void PlayerMove()
     {
-        Vector3 dir = Vector3.right;
         float moveDir = Input.GetAxis("Horizontal");
+        Vector3 dir = moveDir * Vector3.right;
 
-        transform.position += dir * moveDir * moveSpeed * Time.deltaTime;
+        if (moveDir < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            dir = moveDir * Vector3.right;
+            transform.Translate(dir * -1 * moveSpeed * Time.deltaTime);
+        }
+        else if (moveDir > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            dir = moveDir * Vector3.right;
+            transform.Translate(dir * moveSpeed * Time.deltaTime);
+        }
     }
+
+    void PlayerJump()
+    {
+        if (isJumping == false)
+        {
+            playerRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isJumping = true;
+            Debug.Log("JumpingState : " + isJumping);
+        }
+        else return;
+    }
+
 
     void OnDrawGizmos()
     {
