73083ed3ec2a9ee026834db97b39d3658ee8a965
SeungYun-58 <82879029+SeungYun-58@users.noreply.github.com>
2023-09-16 00:05:12 +0900

    FIX Player폴더 추가

--- XRContents8TeamProject/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ XRContents8TeamProject/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using EnemyScripts;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerTest : MonoBehaviour
