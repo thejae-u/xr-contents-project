94e6ae681a18b7122baf2420e56e05c06f304a37
SeungYun-58 <82879029+SeungYun-58@users.noreply.github.com>
2023-10-09 15:54:27 +0900

    FIX NormalEnemy,EliteEnemy DiscountHP 불러오기 추가

--- XRContents8TeamProject/Assets/Scripts/CameraController.cs
+++ XRContents8TeamProject/Assets/Scripts/CameraController.cs
@@ -2,18 +2,14 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
     public Transform target;
     private float trackSpeed = 10;
 
-
-    // Set target
     public void SetTarget(Transform t)
     {
         target = t;
     }
 
-    // Track target
     void LateUpdate()
     {
         if (target)
--- XRContents8TeamProject/Assets/Scripts/PlayerScripts/Bullet.cs
+++ XRContents8TeamProject/Assets/Scripts/PlayerScripts/Bullet.cs
@@ -7,6 +7,7 @@ public class Bullet : MonoBehaviour
  
     private GameObject playerManager;
     private NEnemyController nEnemyController;
+    private EEnemyController eEnemyController;
 
     void Start()
     {
@@ -24,13 +25,21 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        if (collision.gameObject.CompareTag("NormalEnemy"))
         {           
             BulletDestroy();
 
             nEnemyController = collision.GetComponent<NEnemyController>();
             float damage = playerManager.GetComponent<PlayerManager>().playerAtk;
             nEnemyController.DiscountHp(damage);
+        }
+        else if(collision.gameObject.CompareTag("EliteEnemy"))
+        {
+            BulletDestroy();
+            
+            eEnemyController = collision.GetComponent<EEnemyController>();
+            float damege = playerManager.GetComponent<PlayerManager>().playerAtk;
+            eEnemyController.DiscountHp(damege);
         }
         else if (collision.gameObject.CompareTag("Ground"))
         {
