5caaaf8c3e9750bc6befd692be40caa211b4083f
Jae_U <wodn040574@gmail.com>
2023-10-10 22:27:42 +0900

    ADD+FIX 카메라 스탑 구현

--- XRContents8TeamProject/Assets/Scripts/SystemScripts/CameraController.cs
+++ XRContents8TeamProject/Assets/Scripts/SystemScripts/CameraController.cs
@@ -7,6 +7,8 @@ public class CameraController : MonoBehaviour
     public bool IsCameraStop { get; set; }
     private float trackSpeed = 10;
 
+    private Vector3 prevPosition;
+
     public void SetTarget(Transform t)
     {
         target = t;
@@ -14,9 +16,15 @@ public class CameraController : MonoBehaviour
 
     void LateUpdate()
     {
+        if (IsCameraStop)
+        {
+            transform.position = prevPosition;
+        }
+        
         if (target)
         {
             var v = transform.position;
+            prevPosition = v;
             v.x = target.position.x + 5.5f;
             transform.position = Vector3.MoveTowards(transform.position, v, trackSpeed * Time.deltaTime);
         }
--- XRContents8TeamProject/Assets/Scripts/SystemScripts/CameraStopper.cs
+++ XRContents8TeamProject/Assets/Scripts/SystemScripts/CameraStopper.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,38 @@ using UnityEngine;
 public class CameraStopper : MonoBehaviour
 {
     public List<GameObject> stopSections;
+
+    private CameraController cameraController;
+    
+    private int curCount;
+    private int curSection;
+
+    private void Start()
+    {
+        cameraController = GameObject.Find("Main Camera").GetComponent<CameraController>();
+    }
+
+    private void Update()
+    {
+        if (cameraController.IsCameraStop)
+        {
+            CheckEnemy();
+        }
+    }
     
     
+
+    public void StopCamera(int section)
+    {
+        cameraController.IsCameraStop = true;
+        curSection = section;
+    }
+
+    private void CheckEnemy()
+    {
+        if (stopSections[curSection].transform.childCount == 0)
+        {
+            cameraController.IsCameraStop = false;
+        }
+    }
 }
