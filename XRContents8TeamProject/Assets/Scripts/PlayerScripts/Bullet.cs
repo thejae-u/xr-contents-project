b1d839e8f2ad5cc0f888b36ed5b300d0ba9dfdcb
SeungYun-58 <82879029+SeungYun-58@users.noreply.github.com>
2023-10-19 00:32:00 +0900

    FIX Bullet 이동 삭제

--- XRContents8TeamProject/Assets/Scripts/PlayerScripts/Bullet.cs
+++ XRContents8TeamProject/Assets/Scripts/PlayerScripts/Bullet.cs
@@ -2,25 +2,14 @@ using UnityEngine;
 using EnemyScripts;
 
 public class Bullet : MonoBehaviour
-{
-    private Vector3 initialPosition;
- 
+{ 
     private GameObject playerManager;
     private NEnemyController nEnemyController;
     private EEnemyController eEnemyController;
 
     void Start()
     {
-        initialPosition = transform.position;
         playerManager = GameObject.Find("Player");
-    }
-
-    void Update()
-    {
-        if (Vector3.Distance(initialPosition, transform.position) > playerManager.GetComponent<PlayerManager>().fireDistance)
-        {
-            BulletDestroy();
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
