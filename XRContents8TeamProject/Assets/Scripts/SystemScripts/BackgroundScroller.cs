f03bf127d43bb03a0188763ec8783df81b0a9861
Jae_U <wodn040574@gmail.com>
2023-10-28 01:36:35 +0900

    ADD+FIX 배경 로직 변경에 따른 설정 수정

--- XRContents8TeamProject/Assets/Scripts/SystemScripts/BackgroundScroller.cs
+++ XRContents8TeamProject/Assets/Scripts/SystemScripts/BackgroundScroller.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class BackgroundScroller : MonoBehaviour
 {
-    
+    /*
     public float x;
     public float y;
 
@@ -21,5 +22,38 @@ public class BackgroundScroller : MonoBehaviour
         pos.x = pos.x * x / 50;
         pos.y = pos.y * y / 50;
         mat.SetVector("_att", pos);
+    }*/
+
+    [SerializeField] private List<Transform> sprites;
+
+    private Vector2 camPos;
+    private Vector2 prevPos;
+    
+    // True : Right, False : Left
+    private bool moveDir;
+
+    private void Start()
+    {
+        prevPos = Camera.main.transform.position;
+    }
+
+
+    private void Update()
+    {
+        camPos = Camera.main.transform.position;
+
+        // Camera Moving Check
+        if (camPos == prevPos) return;
+        
+        Vector2 dir = (prevPos - camPos).normalized;
+        float dist = (prevPos - camPos).magnitude;
+        moveDir = dir.x > 0;
+
+        foreach (var sprite in sprites)
+        {
+            
+        }
+
+        prevPos = camPos;
     }
 }
