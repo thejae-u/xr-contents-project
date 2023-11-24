ee470431ace42af17fb4065a4e13f559cd183c36
SeungYun-58 <82879029+SeungYun-58@users.noreply.github.com>
2023-11-24 18:08:20 +0900

    ADD 사운드 옵션 창 및 사운드 조절

---
+++ XRContents8TeamProject/Assets/Scripts/SystemScripts/SoundModulate.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundModulate : MonoBehaviour
+{
+    public AudioMixer mixer;
+
+    public void AudioController(float sliderVal)
+    {
+        mixer.SetFloat("Master", Mathf.Log10(sliderVal) * 20);
+    }
+
+    // 버튼 하나로 소리 끄기
+    // Audio입력받는 쪽(AudioListener)에서 볼륨을 줄이는 것
+    //public void ToggleAudioVolum()
+    //{  
+    //    AudioListener.volume = AudioListener.volume == 0 ? 1 : 0;
+    //}
+}
