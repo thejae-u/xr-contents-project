bccf2d14dc506b2a06eceaf99fc1156496cc005c
Jae_U <wodn040574@gmail.com>
2023-11-28 00:39:26 +0900

    FIX 사운드 매니저 변경

--- XRContents8TeamProject/Assets/Scripts/SystemScripts/SoundManager.cs
+++ XRContents8TeamProject/Assets/Scripts/SystemScripts/SoundManager.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Audio;
+using Random = UnityEngine.Random;
 
 public enum EPlayType
 {
@@ -8,19 +11,31 @@ public enum EPlayType
     Loop
 }
 
+public enum ESoundType
+{
+    Bgm,
+    Sfx
+}
+
 
 [Serializable]
 public class Sound
 {
+    [HideInInspector] public float soundVolume;
     public string soundName;
     public AudioClip soundData;
     public EPlayType playType;
+    public ESoundType soundType;
 }
     
 public class SoundManager : MonoBehaviour
 {
     // Sound
-    public GameObject playPrefab;
+    public GameObject bgmPlayPrefab;
+    public GameObject sfxPlayPrefab;
+
+    public AudioMixer mixer;
+    
     [SerializeField] private List<Sound> sounds;
     private List<GameObject> createdObjs;
 
@@ -34,7 +49,11 @@ public class SoundManager : MonoBehaviour
 
     private void Awake()
     {
-        inst = this;
+        if (inst == null)
+            inst = this;
+        else
+            Destroy(gameObject);   
+        DontDestroyOnLoad(gameObject);
     }
     
     #endregion
@@ -56,19 +75,30 @@ public class SoundManager : MonoBehaviour
         return null;
     }
 
-    public void Play(string soundName, GameObject obj)
+    public void Play(string soundName)
     {
         var clip = Find(soundName);
         if (clip == null) return;
+        GameObject newObj;
 
-        var newObj = Instantiate(playPrefab, obj.transform);
+        switch (clip.soundType)
+        {
+            case ESoundType.Bgm:
+                newObj = Instantiate(bgmPlayPrefab, transform);
+                break;
+            case ESoundType.Sfx:
+                newObj = Instantiate(sfxPlayPrefab, transform);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
 
         newObj.name = $"{soundName} {sid++}";
         newObj.transform.GetComponent<PlayScript>().Data.soundName = clip.soundName;
         newObj.transform.GetComponent<PlayScript>().Data.soundData = clip.soundData;
         newObj.transform.GetComponent<PlayScript>().Data.playType = clip.playType;
         newObj.transform.GetComponent<PlayScript>().Play();
-        
+
         createdObjs.Add(newObj);
     }
 
@@ -80,8 +110,46 @@ public class SoundManager : MonoBehaviour
            {
                var delObj = createdObjs[i];
                createdObjs.Remove(createdObjs[i]);
-               Destroy(delObj);
+               if (obj)
+                   Destroy(delObj);
            }
        }
+    }
+
+    public void DeleteAllSound()
+    {
+        foreach (var obj in createdObjs)
+        {
+            Destroy(obj);
+        }    
+    }
+    
+    public void SoundFadeOut()
+    {
+        float volume;
+        mixer.GetFloat("Master", out volume);
+        Debug.Log(volume);
+        mixer.DOSetFloat("Master", -80.0f, 2.0f).OnComplete(() =>
+        {
+            mixer.SetFloat("Master", volume);
+        });
+    }
+
+    public void PlayBombSound()
+    {
+        switch (Random.Range(0, 3))
+        {
+            case 0:
+                Play("Explosion1");
+                break;
+            case 1:
+                Play("Explosion2");
+                break;
+            case 2:
+                Play("Explosion3");
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }
